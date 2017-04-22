5293c283b0ccc7b62c3415b3d47e71c41eaf2cad
Siyuan Ding <dsyff@hotmail.com>
2017-04-21 21:52:08 -0400

    nothing

--- Calculations.java
+++ Calculations.java
@@ -1,10 +1,29 @@
 public class Calculations {
   
-  private static final double earthRadius = 6371e3;
-    public double distanceBetween (Location pt1, Location pt2) {
-     return 1.0;
+  private static final double earthRadius;
+
+  static {
+    earthRadius = 6.371e6;
   }
-
-
-
+  
+  private static double sine (double degrees) {return Math.sin (Math.toRadians (degrees));}
+  private static double cosine (double degrees) {return Math.cos (Math.toRadians (degrees));}
+  
+  public static double distanceBetween(Location pt1, Location pt2) {
+    double latDiff = pt2.lat - pt1.lat;
+    double lngDiff = pt2.lng - pt1.lng;
+    double haversine = Math.pow(sine(latDiff/2), 2.0) + cosine(pt1.lat) * cosine(pt2.lat) * Math.pow(sine(lngDiff/2), 2);
+    double unitDistance = 2 * Math.atan2(Math.sqrt(haversine), Math.sqrt(1 - haversine));
+    return earthRadius * unitDistance;
+  }
+  
+  public static void main(String[] args) {
+    
+    Location gasson = new Location (42.335573, -71.170486);
+    Location maloney = new Location (42.336108, -71.168435);
+    System.out.println(gasson.lat);
+    System.out.println(distanceBetween(gasson, maloney));
+    
+  }
+  
 }
