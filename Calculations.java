cc82b373386366f7ba7c705f2bfdaf0ff3f8eac0
Siyuan Ding <dsyff@hotmail.com>
2017-04-22 01:37:50 -0400

    nothing

--- Calculations.java
+++ Calculations.java
@@ -1,13 +1,15 @@
 public class Calculations {
   
   private static final double earthRadius;
-  private static final int lunchTime; //in minutes since
+  private static final int lunchTime; //preferred lunch start time, in minutes since
   private static final int lunchPeriod; //minutes
+  private static final double walkingSpeed; //in meters per minute
 
   static {
     earthRadius = 6.371e6;
-    lunchTime = 1440;
+    lunchTime = 720;
     lunchPeriod = 30;
+    walkingSpeed = 84.0 / Math.sqrt(2.0);
   }
   
   private static double sine (double degrees) {return Math.sin (Math.toRadians (degrees));}
@@ -21,28 +23,49 @@ public class Calculations {
     return earthRadius * unitDistance;
   }
   
-  
-  private static boolean isValid(Location from, Location lunch, Location to) {
-  //not implemented
-    return true;
-  }
-  
-  private static eventBefore(Event startEvent, int time) {
-  
-  
+  private static double travelTime(Location from, Location to) {
+    return distanceBetween(from, to) / walkingSpeed;
   }
   
   public static Event lunchSuggestion(Event startEvent) {
-    lunchEvent = new Event();//ask andre for how to create event
     if (startEvent == null) return null; //no classes, no lunch suggestion
     Event currentEvent = startEvent;
-    int lunchStart = 0;
+    int lunchStartDiff = 1440;
+    Event bestLunchEvent;
+    Event bestAfterEvent;
     while (currentEvent.nextEvent != null) {
-      nearest
+      Location from = currentEvent.building.getLocation();
+      Location to = currentEvent.nextEvent.building.getLocation();
+      DiningHall closestDiningHall = BuildingUpLocations.getNearest(from, to);
+      Location meal = DiningHall.getLocation();
+      double toTime = travelTime(from, meal);
+      double awayTime = travelTime(away, meal);
+      double totalTime = toTime + awayTime + lunchPeriod;
+      int gapEnd = currentEvent.nextEvent.startMinSince12;
+      int gapStart = currentEvent.endMinSince12;
+      int lunchStart;
+      if (totalTime <= gapEnd - gapStart) {
+        if (gapEnd < lunchTime) lunchStart = (int) (gapEnd - awayTime - lunchPeriod);
+        else if (gapStart > lunchTime) lunchStart = (int) (gapStart  + toTime);
+        else if (lunchTime + lunchPeriod + awayTime > gapEnd) lunchStart = (int) (gapEnd - awayTime - lunchPeriod);
+        else if (lunchTime - toTime < gapStart) lunchStart = (int) (gapStart + toTime);
+        else lunchStart = lunchTime;
+        if (Math.abs(lunchTime - lunchStart) < lunchStartDiff) {
+          bestLunchEvent = new Event(lunchStart, lunchStart + lunchPeriod, closestDiningHall, "lunch", currentEvent, currentEvent.nextEvent);
+          bestAfterEvent = currentEvent;
+        }
+        if (lunchStartDiff == 0) break;
+      }
       currentEvent = currentEvent.nextEvent;
     }
     
-  
+    if (bestLunchEvent != null) {
+      bestAfterEvent.nextEvent.previousEvent = bestLunchEvent;
+      bestAfterEVent.nextEvent = bestLunchEvent;
+    }
+    
+    return startEvent;
+    
   }
 
 }
